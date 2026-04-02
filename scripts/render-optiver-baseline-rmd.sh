a9ca4fc58e56fbdc33b9092b64f8458b20b30849
Bob Jansen <bobjansen@gmail.com>
2026-04-02 19:17:43 +0200

    Trimming the notebook

--- scripts/render-optiver-baseline-rmd.sh
+++ scripts/render-optiver-baseline-rmd.sh
@@ -8,6 +8,6 @@ cd "${repo_root}"
 output_format="${1:-html_document}"
 build_dir="${IBEX_BUILD_DIR:-${repo_root}/build-release}"
 
-cmake --build "${build_dir}" --parallel --target ibex_runtime
+cmake --build "${build_dir}" --parallel --target ibex_runtime ibex_parquet_plugin ibex_lightbm_plugin
 
 Rscript -e "renv::load('${repo_root}'); install.packages('${repo_root}/r/ribex', repos = NULL, type = 'source', INSTALL_opts = '--preclean'); rmarkdown::render('notebooks/baseline-lgb-xgb-and-catboost.Rmd', output_format='${output_format}')"

b221e03ab6b855cad72eaaa867353de9c3a9a9b5
Bob Jansen <bobjansen@gmail.com>
2026-04-03 15:35:21 +0200

    Padding is fine

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -61,6 +61,7 @@ target_compile_options(ibex_compiler_options
             /Zc:__cplusplus
             /EHsc
             /wd4189
+            /wd4324
             /wd4702
             /D_CRT_SECURE_NO_WARNINGS
         >
